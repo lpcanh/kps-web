167a3990e8b9ee5e47698460cdad21f5ae392007
Le Nguyen Corp <lephuoccanh@gmail.com>
2016-02-23 18:28:19 +0000

    Update Md Slider

---
+++ web/sites/all/modules/block_background_color/colorpicker/update.js
@@ -0,0 +1,7 @@
+    function createForm()
+    {
+        document.getElementById("formContainer").innerHTML = 
+'<input type="text" id="cinput2" class="color" value="#00aa00" />';
+        MC.ColorPicker.reload();
+    }
+
